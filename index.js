33f1f3389e6d147e4e495709fc2df32794b7b0d6
Paulo Andrade <paulo.andrade@accurate.com.br>
2021-12-29 12:40:03 -0300

    feature 1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const somaUmMaisUm = () => 1 +1
+const SomaUmMaisUm = () => 1 +1
 
-const multiplicaDoisVezesUm = () => 2*1
+const MultiplicaDoisVezesUm = () => 2*1
 
 const main = () =>{
-    console.log(somaUmMaisUm())
+    console.log(SomaUmMaisUm())
 
-    console.log(multiplicaDoisVezesUm())
+    console.log(MultiplicaDoisVezesUm())
 
 }
 main()
